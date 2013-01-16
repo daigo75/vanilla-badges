8d614210bd97641aca5887977b0601f2ae5ea242
Diego Zanella <businessdad75@gmail.com>
2013-01-16 00:40:26 +0000

    Added Count types

--- Awards/lib/classes/rules/postcountrule/class.postcountrule.php
+++ Awards/lib/classes/rules/postcountrule/class.postcountrule.php
@@ -15,6 +15,15 @@ AwardRulesManager::RegisterRule('PostCountRule',
  * Post Count Award Rule.
  */
 class PostCountRule extends BaseAwardRule {
+	/* @var array Contains the types of calculation available for the Rule
+	 * - At: Rule returns true when the specified threshold is reached.
+	 * - Every: Rule returns true whenever the specified amount of content type is reached.
+	 *
+	 * Note: variable cannot be initialized on declaration because it makes use of
+	 * T() for translations.
+	 */
+	public static $CountTypes;
+
 	/**
 	 * Runs the processing of the Rule, which will return how many times the Award
 	 * should be assigned to the User, based on the specified configuration.
@@ -23,6 +32,12 @@ class PostCountRule extends BaseAwardRule {
 	 */
 	public function Process($UserID, $RuleConfig, array $EventInfo = null) {
 		return self::NO_ASSIGMENTS;
+	}
+
+	public function __construct() {
+		parent::__construct();
+		self::$CountTypes = array(1 => T('At'),
+															2 => T('Every'),);
 	}
 
 	// TODO Add View to configure the Rule
