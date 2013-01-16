1422f84d8110a189db29e6353ae92c4066df33d9
Diego Zanella <businessdad75@gmail.com>
2013-01-16 00:38:10 +0000

    Implemented configuration interface for Discussion and Comments Counts

--- Awards/lib/classes/rules/postcountrule/views/settings_view.php
+++ Awards/lib/classes/rules/postcountrule/views/settings_view.php
@@ -5,16 +5,31 @@
 
 ?>
 <div class="Rule">
-	<span>PostCount Rule Settings</span>
 	<ul>
 		<li>
+			<?php
+			// TODO Implement interface for "at/every" X questions
+			// TODO Implement interface for "at/every" X comments
+			// TODO Implement interface for "at/every" X answers
+			// TODO Implement interface for "at/every" X of anything
+				echo $this->Form->CheckBox('PostCount_Discussions_Enabled', T('Discussions posted by the User'));
+				echo $this->Form->DropDown('PostCount_Discussions_CountType', PostCountRule::$CountTypes);
+				echo $this->Form->TextBox('PostCount_Discussions_Amount',
+																	array('class' => 'InputBox Numeric'));
+				echo Wrap(T('Discussions'));
+			?>
+		</li>		<li>
 			<?php
 			// TODO Implement interface for "at/every" X discussions
 			// TODO Implement interface for "at/every" X questions
 			// TODO Implement interface for "at/every" X comments
 			// TODO Implement interface for "at/every" X answers
 			// TODO Implement interface for "at/every" X of anything
-				echo $this->Form->CheckBox('Test');
+				echo $this->Form->CheckBox('PostCount_Comments_Enabled', T('Comments posted by the User'));
+				echo $this->Form->DropDown('PostCount_Comments_CountType', PostCountRule::$CountTypes);
+				echo $this->Form->TextBox('PostCount_Comments_Amount',
+																	array('class' => 'InputBox Numeric'));
+				echo Wrap(T('Comments'));
 			?>
 		</li>
 	</ul>
