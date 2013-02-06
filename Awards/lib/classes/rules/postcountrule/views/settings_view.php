6d296ee60e59497b26d327df7633fee3a2bb7ffd
Diego Zanella <businessdad75@gmail.com>
2013-02-06 23:24:53 +0000

    Replaced "echo" statements with calls to Rule's RenderRuleField() method

--- Awards/lib/classes/rules/postcountrule/views/settings_view.php
+++ Awards/lib/classes/rules/postcountrule/views/settings_view.php
@@ -7,30 +7,32 @@
 <div class="Rule">
 	<ul>
 		<li>
+			<div class="Discussions">
 			<?php
-			// TODO Implement interface for "at/every" X questions
-			// TODO Implement interface for "at/every" X comments
-			// TODO Implement interface for "at/every" X answers
-			// TODO Implement interface for "at/every" X of anything
-				echo $this->Form->CheckBox('PostCount_Discussions_Enabled', T('Discussions posted by the User'));
-				echo $this->Form->DropDown('PostCount_Discussions_CountType', PostCountRule::$CountTypes);
-				echo $this->Form->TextBox('PostCount_Discussions_Amount',
-																	array('class' => 'InputBox Numeric'));
-				echo Wrap(T('Discussions'));
+				echo Wrap('Discussion', 'h3');
+				PostCountRule::RenderRuleField($this->Form->CheckBox('Discussions_Enabled', T('Discussions posted by the User')));
+				//echo $this->Form->CheckBox('PostCount_Discussions_Enabled', T('Discussions posted by the User'));
+				PostCountRule::RenderRuleField($this->Form->DropDown('Discussions_CountType', PostCountRule::$CountTypes));
+				PostCountRule::RenderRuleField($this->Form->TextBox('Discussions_Amount',
+																														array('class' => 'InputBox Numeric')));
 			?>
-		</li>		<li>
+			</div>
+		</li>
+		<li>
+			<div class="Comments">
 			<?php
-			// TODO Implement interface for "at/every" X discussions
-			// TODO Implement interface for "at/every" X questions
-			// TODO Implement interface for "at/every" X comments
-			// TODO Implement interface for "at/every" X answers
-			// TODO Implement interface for "at/every" X of anything
-				echo $this->Form->CheckBox('PostCount_Comments_Enabled', T('Comments posted by the User'));
-				echo $this->Form->DropDown('PostCount_Comments_CountType', PostCountRule::$CountTypes);
-				echo $this->Form->TextBox('PostCount_Comments_Amount',
-																	array('class' => 'InputBox Numeric'));
+				echo Wrap('Comments', 'h3');
+				PostCountRule::RenderRuleField($this->Form->CheckBox('Comments_Enabled', T('Comments posted by the User')));
+				PostCountRule::RenderRuleField($this->Form->DropDown('Comments_CountType', PostCountRule::$CountTypes));
+				PostCountRule::RenderRuleField($this->Form->TextBox('Comments_Amount',
+																														array('class' => 'InputBox Numeric')));
 				echo Wrap(T('Comments'));
 			?>
+			</div>
 		</li>
+		<?php
+			// TODO Implement interface for "at/every" X questions
+			// TODO Implement interface for "at/every" X answers
+		?>
 	</ul>
 </div>
