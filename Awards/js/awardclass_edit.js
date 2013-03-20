1fd46d82206e87b15802c0e223dc618449b61799
Diego Zanella <businessdad75@gmail.com>
2013-03-20 00:30:08 +0000

    Removed CodeMirror as it wouldn't highlight CSS correctly anyway
    - The UI requires that no curly braces or CSS identifiers are used. This results in an invalid CSS syntax, which only becomes valid when the awardclasses.css file is generated.

--- Awards/js/awardclass_edit.js
+++ Awards/js/awardclass_edit.js
@@ -1,12 +1,12 @@
 jQuery(document).ready(function(){
-	// Load the syntax highlighter for the CSS textarea
-	var ClassCSSEditor = CodeMirror.fromTextArea(document.getElementById('Form_AwardClassCSS'),
-																							 {
-																									lineNumbers: true
-																							 });
-	$(".CodeMirror").resizable({
-      resize: function() {
-				ClassCSSEditor.setSize($(this).width(), $(this).height());
-      }
-	});
+//	// Load the syntax highlighter for the CSS textarea
+//	var ClassCSSEditor = CodeMirror.fromTextArea(document.getElementById('Form_AwardClassCSS'),
+//																							 {
+//																									lineNumbers: true
+//																							 });
+//	$(".CodeMirror").resizable({
+//      resize: function() {
+//				ClassCSSEditor.setSize($(this).width(), $(this).height());
+//      }
+//	});
 });
