dfc7f24a867ec8180b2c6e4ee53f8f3f8854f651
Diego Zanella <businessdad75@gmail.com>
2013-04-15 12:17:34 +0100

    Updated version

--- Awards/class.awards.plugin.php
+++ Awards/class.awards.plugin.php
@@ -14,7 +14,7 @@ require(AWARDS_PLUGIN_LIB_PATH . '/awards.validation.php');
 $PluginInfo['Awards'] = array(
 	'Name' => 'Awards Plugin',
 	'Description' => 'Awards Plugin for Vanilla Forums',
-	'Version' => '13.04.08 beta',
+	'Version' => '13.04.15 beta',
 	'RequiredApplications' => array('Vanilla' => '2.0'),
 	'RequiredTheme' => FALSE,
 	'RequiredPlugins' => array('Logger' => '12.10.28',
