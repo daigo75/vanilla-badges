440070af66f8255ae78e4b0466820fc9d7c7df92
Diego Zanella <businessdad75@gmail.com>
2013-04-20 01:09:57 +0100

    Fixed bug in variable name

--- Awards/views/awards_export_view.php
+++ Awards/views/awards_export_view.php
@@ -73,7 +73,7 @@
 		<div id="Output" class="clearfix <?php echo $OutputCssClass; ?>">
 			<div class="Header">
 				<?php
-					$ResultMessage = ($ImportResult === AWARDS_OK) ? T('Success.') : T('Failure.');
+					$ResultMessage = ($ExportResult === AWARDS_OK) ? T('Success.') : T('Failure.');
 					echo Wrap(T('Export completed.') . ' ' . $ResultMessage, 'h2');
 					if(isset($ExportResult) && ($ExportResult !== AWARDS_OK)) {
 						echo Wrap(sprintf(T('Operation failed. Export result code: %d.'),
