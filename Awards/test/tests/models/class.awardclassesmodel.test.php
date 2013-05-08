3b2d8c5bab60f2b4d145191f8451c93a7c633a7b
Diego Zanella <businessdad75@gmail.com>
2013-05-08 16:12:50 +0100

    Added test for Award Class model

---
+++ Awards/test/tests/models/class.awardclassesmodel.test.php
@@ -0,0 +1,47 @@
+<?php
+/**
+{licence}
+*/
+
+class AwardClassesModelTests extends PHPUnit_Vanilla_TestCase {
+	/**
+	 * Returns some Award Class Data that can be used to save an Award Class. Each
+	 * test can modify such data to save more tha one Award Class.
+	 *
+	 * @return array An array describing an Award Class.
+	 */
+	private function _SampleAwardClassData() {
+		return array(
+			'AwardClassID' => 1,
+			'AwardClassName' => 'Test Award Class ',
+			'AwardClassDescription' => 'Test Award Class 1 - Description',
+			'AwardClassImageFile' => 'plugins/Awards/design/images/awardclasses/dummyfile.png',
+			'AwardClassCSS' => '',
+		);
+	}
+
+	/**
+	 * Test Suite initialization.
+	 */
+	protected function setUp() {
+		$this->AwardClassesModel = new AwardsModel();
+	}
+
+	/**
+	 * Test Suite finalization and cleanup.
+	 */
+	protected function tearDown() {
+		unset($this->AwardClassesModel);
+	}
+
+	/**
+	 * Sample test. Verify that internal Plugin variable has been initialized.
+	 */
+	public function testAwardInsert() {
+		$AwardClassData = $this->_SampleAwardClassData();
+		unset($AwardClassData['AwardClassID']);
+
+		$NewAwardClassID = $this->AwardClassesModel->Save($AwardClassData);
+		$this->assertTrue(is_numeric($NewAwardClassID));
+	}
+}
